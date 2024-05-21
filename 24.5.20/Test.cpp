bca4dcd7efcf6697006afe8072cdab8c9d65b04a
Zhang Jiahui <shuaigejiahui@163.com>
2024-05-21 16:47:31 +0800

    添加项目文件。

---
+++ 24.5.20/Test.cpp
@@ -0,0 +1,130 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+class Date
+{
+public:
+	int GetMonthDay(int year, int month)
+	{
+		int MonthDays[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+			return 29;
+		return MonthDays[month];
+	}
+
+	Date(int year = 0, int month = 1, int day = 1)
+	{
+		if (year >= 0
+			&& month >= 1 && month <= 12
+			&& day >= 1 && day <= GetMonthDay(year, month))
+		{
+			_year = year;
+			_month = month;
+			_day = day;
+		}
+		else
+			cout << "非法日期" << endl;
+	}
+
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+
+	bool operator<(const Date& d)
+	{
+		if (_year < d._year)
+			return true;
+		else if (_year == d._year && _month < d._month)
+			return true;
+		else if (_year == d._year && _month == d._month && _day < d._day)
+			return true;
+
+		return false;
+	}
+
+	bool operator==(const Date& d)
+	{
+		return _year == d._year && _month == d._month && _day == d._day;
+	}
+
+	bool operator<=(const Date& d)
+	{
+		return *this < d || *this == d;
+	}
+
+	bool operator>(const Date& d)
+	{
+		return !(*this <= d);
+	}
+
+	bool operator>=(const Date& d)
+	{
+		return !(*this < d);
+	}
+
+	bool operator!=(const Date& d)
+	{
+		return !(*this == d);
+	}
+
+	Date operator+(int day)
+	{
+		Date ret(*this);
+		ret._day += day;
+
+		while (ret._day > GetMonthDay(ret._year, ret._month))
+		{
+			ret._day -= GetMonthDay(ret._year, ret._month);
+			ret._month++;
+
+			if (ret._month == 13)
+			{
+				ret._year++;
+				ret._month = 1;
+			}
+		}
+
+		return ret;
+	}
+
+	Date& operator+=(int day)
+	{
+		_day += day;
+		while (_day > GetMonthDay(_year, _month))
+		{
+			_day -= GetMonthDay(_year, _month);
+			_month++;
+			if (_month == 13)
+			{
+				_year++;
+				_month = 1;
+			}
+		}
+		
+		return *this;
+	}
+
+	void Print()
+	{
+		cout << _year << "." << _month << "." << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1(2020, 4, 11);
+	
+	Date d4 = d1 + 100;
+	d4.Print();
+	d1 += 10;
+	d1.Print();
+	return 0;
+}
